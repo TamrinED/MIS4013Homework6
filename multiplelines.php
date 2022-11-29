534ddba45baffee5c95d333cf76ca46c06e07c89
TamrinED <98431860+TamrinED@users.noreply.github.com>
2022-11-29 05:18:27 -0600

    Update multiplelines.php

--- multiplelines.php
+++ multiplelines.php
@@ -1,5 +1,4 @@
-<!DOCTYPE html>
-<html>
+<?php require_once"header.php"?>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
 <body>
 <canvas id="myChart" style="width:100%;max-width:600px"></canvas>
